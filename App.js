f0aaaac2eeeb34881eeb50f4408fce1e6262f2c2
Linh Huynh <70009294+linh2020@users.noreply.github.com>
2022-05-13 22:38:26 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 function App() {
@@ -9,11 +9,20 @@ function App() {
 
   //https://reactjs.org/docs/hooks-reference.html#usestate
   //https://codegino.com/blog/react-setstate-callback-function
+
+  // const handleCounter = () => {
+  //   setTimeout(
+  //     () => { setCounter((prevCounter) => prevCounter + 1) },
+  //     0);
+  // }
   const handleCounter = () => {
-    setTimeout(
-      () => { setCounter((prevCounter) => prevCounter + 1) },
-      0);
-  }
+    setCounter(counter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    // console.log(counter);
+  };
+
+  //https://medium.com/@sanjeewafern/perform-an-immediate-action-after-setting-state-on-a-state-variable-with-react-functional-component-e9e1526f655a
+  useEffect(() => { console.log(counter); })
 
   return (
     <div style={{ padding: '100px' }}>
