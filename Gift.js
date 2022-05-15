503a928987fd8b8ea45635162d97c349b1b03d18
Linh Huynh <70009294+linh2020@users.noreply.github.com>
2022-05-14 23:41:26 -0700

    Update Gift.js

--- Gift.js
+++ Gift.js
@@ -3,24 +3,46 @@ import { useState } from "react";
 function App() {
 
   const gifts = ['CPU', 'VGA', 'Memory'];
-
   const [gift, setGift] = useState("Waiting")
 
-  const randomNumber = () => Math.floor(Math.random() * 3);
-
   const handleGift = () => {
-    setGift(() => {
-      return gifts[randomNumber()];
-    });
+    const index = Math.floor(Math.random() * 3);
+    setGift(() => gifts[index]);
   }
-
 
   return (
     <div style={{ padding: '20px' }}>
-      <h2>{gift}</h2>
+      <h2>{gift || 'Waiting!'}</h2>
       <button onClick={handleGift}> Get a gift</button>
     </div>
   );
 }
 
 export default App
+
+// import { useState } from "react";
+
+// function App() {
+
+//   const gifts = ['CPU', 'VGA', 'Memory'];
+
+//   const [gift, setGift] = useState("Waiting")
+
+//   const randomNumber = () => Math.floor(Math.random() * 3);
+
+//   const handleGift = () => {
+//     setGift(() => {
+//       return gifts[randomNumber()];
+//     });
+//   }
+
+
+//   return (
+//     <div style={{ padding: '20px' }}>
+//       <h2>{gift}</h2>
+//       <button onClick={handleGift}> Get a gift</button>
+//     </div>
+//   );
+// }
+
+// export default App
