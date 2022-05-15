af45f8e3e788d09ba983da030dd9e02dd79c7132
Linh Huynh <70009294+linh2020@users.noreply.github.com>
2022-05-14 23:34:24 -0700

    Create Gift.js

---
+++ Gift.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+
+  const gifts = ['CPU', 'VGA', 'Memory'];
+
+  const [gift, setGift] = useState("Waiting")
+
+  const randomNumber = () => Math.floor(Math.random() * 3);
+
+  const handleGift = () => {
+    setGift(() => {
+      return gifts[randomNumber()];
+    });
+  }
+
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>{gift}</h2>
+      <button onClick={handleGift}> Get a gift</button>
+    </div>
+  );
+}
+
+export default App
